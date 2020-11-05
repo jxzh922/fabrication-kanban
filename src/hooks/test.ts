a2b71f6890494d83e406d33255290fa472d5b46f
焦现震 <jxzh922@163.com>
2020-11-05 16:21:05 +0800

    添加调试信息 修改数据库连接配置（测试git）

---
+++ src/hooks/test.ts
@@ -0,0 +1,12 @@
+// Use this hook to manipulate incoming or outgoing data.
+// For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
+import { Hook, HookContext } from '@feathersjs/feathers';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default (options = {}): Hook => {
+  return async (context: HookContext): Promise<HookContext> => {
+    //context.a = 1;
+
+    return context;
+  };
+};
